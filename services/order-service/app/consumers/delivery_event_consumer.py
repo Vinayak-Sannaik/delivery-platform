fdf7443ab8762b27ba55990ce4ac259f10723fa2
TV-Vinayak <vinayak.sannaik@techverito.com>
2026-07-30 16:05:30 +0530

    log: delivery event consumer in order service add cert

--- services/order-service/app/consumers/delivery_event_consumer.py
+++ services/order-service/app/consumers/delivery_event_consumer.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import ssl
 
 from aiokafka import AIOKafkaConsumer
 
@@ -7,50 +9,63 @@ from app.core.database import AsyncSessionLocal
 from app.repositories.order_repository import OrderRepository
 from app.services.order_service import OrderService
 
-import logging
-
 logger = logging.getLogger(__name__)
 
-class DeliveryEventConsumer:
 
+class DeliveryEventConsumer:
     def __init__(self):
+        kwargs = {
+            "bootstrap_servers": settings.KAFKA_BOOTSTRAP_SERVERS,
+            "group_id": "order-service-delivery-events",
+            "value_deserializer": lambda v: json.loads(v.decode("utf-8")),
+        }
+
+        if settings.KAFKA_SECURITY_PROTOCOL == "SASL_SSL":
+            ssl_context = ssl.create_default_context(
+                cafile=settings.KAFKA_SSL_CA_LOCATION
+            )
+
+            kwargs.update(
+                {
+                    "security_protocol": settings.KAFKA_SECURITY_PROTOCOL,
+                    "sasl_mechanism": settings.KAFKA_SASL_MECHANISM,
+                    "sasl_plain_username": settings.KAFKA_USERNAME,
+                    "sasl_plain_password": settings.KAFKA_PASSWORD,
+                    "ssl_context": ssl_context,
+                }
+            )
+
         self.consumer = AIOKafkaConsumer(
             "delivery-events",
-            bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS,
-            group_id="order-service-delivery-events",
-            value_deserializer=lambda v: json.loads(
-                v.decode("utf-8")
-            ),
+            **kwargs,
         )
 
     async def start(self):
+        logger.info("Starting DeliveryEventConsumer...")
+
         await self.consumer.start()
+
+        logger.info("DeliveryEventConsumer connected to Kafka")
 
         try:
             async for message in self.consumer:
+                logger.info(
+                    "Delivery event received: %s",
+                    message.value,
+                )
 
                 print(
                     "Delivery event received:",
-                    message.value,
-                )
-                logger.info(
-                    "Kafka message received at order service: %s",
                     message.value,
                 )
 
                 event = message.value
 
                 async with AsyncSessionLocal() as db:
-
                     repository = OrderRepository(db)
 
                     service = OrderService(
                         order_repository=repository,
-                    )
-                    
-                    logger.info(
-                        "Delivery assigned created for order: %s",
-                        event.order_id,
                     )
 
                     await service.handle_delivery_event(
@@ -59,5 +74,17 @@ class DeliveryEventConsumer:
 
                     await db.commit()
 
+                    logger.info(
+                        "Processed delivery event for order: %s",
+                        event.get("payload", {}).get("order_id"),
+                    )
+
+        except Exception:
+            logger.exception(
+                "Error while processing delivery events"
+            )
+            raise
+
         finally:
             await self.consumer.stop()
+            logger.info("DeliveryEventConsumer stopped")
