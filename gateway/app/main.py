0981d7c76104a7536693d90ab97867020ac26f09
TV-Vinayak <vinayak.sannaik@techverito.com>
2026-07-31 16:02:34 +0530

    log: add observability

--- gateway/app/main.py
+++ gateway/app/main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI
 from app.core.config import settings
-import logging
+from app.core.logging import setup_logging
 
 from app.routers.auth import router as auth_router
 from app.routers.catalog import router as catalog_router
 from app.routers.order  import router as order_router
 from app.middleware.auth import AuthenticationMiddleware
+from app.middleware.request_id import RequestIdMiddleware
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
-)
+
+setup_logging()
 
 app = FastAPI(
     title="Food Delivery API Gateway",
     version="1.0.0",
 )
 
-# print("url: ", settings.IDENTITY_SERVICE_URL)
+app.add_middleware(RequestIdMiddleware)
 app.add_middleware(AuthenticationMiddleware)
 
 app.include_router(auth_router)
