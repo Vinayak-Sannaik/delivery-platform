0f877aca074727d417d4a206344a4b6500596561
TV-Vinayak <vinayak.sannaik@techverito.com>
2026-07-31 16:27:44 +0530

    log: propagate the X-Request-ID

--- gateway/app/middleware/request_id.py
+++ gateway/app/middleware/request_id.py
@@ -3,10 +3,16 @@ import uuid
 from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from app.core.request_context import request_id_ctx
+
 
 class RequestIdMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
 
+    async def dispatch(
+        self,
+        request: Request,
+        call_next,
+    ):
         request_id = request.headers.get(
             "X-Request-ID",
             str(uuid.uuid4()),
@@ -14,7 +20,12 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
 
         request.state.request_id = request_id
 
-        response = await call_next(request)
+        token = request_id_ctx.set(request_id)
+
+        try:
+            response = await call_next(request)
+        finally:
+            request_id_ctx.reset(token)
 
         response.headers["X-Request-ID"] = request_id
 
